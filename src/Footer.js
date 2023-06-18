74c14ed134ea7d94a7682e6f7bf12e2d549b6925
Rajan GC <gcrajan339@gmail.com>
2023-06-18 10:36:32 +0545

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className="footerText"> 
+        <span>Note:</span> All prices are for Nepalese only. It may differ for SARRC or foreign countries. </p>
+    </div>
+  )
+}
+
+export default Footer
